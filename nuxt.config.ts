811ae002da07d145ca88a67f9e65325d8305e0bc
Bossdeb <150757086+Bossdeb@users.noreply.github.com>
2025-04-21 21:28:41 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css', '~/assets/css/font.css'],
+  css: ['~/assets/css/main.css'],
     runtimeConfig: {
       public: {
         apiBase: process.env.NUXT_PUBLIC_API_BASE,
